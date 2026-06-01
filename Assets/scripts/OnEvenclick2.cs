21a7246fa1e8c66cf7c6f66cc6730fcd079d68c8
Names <40932@ma-web.nl>
2026-06-01 15:41:35 +0200

    die key puzzle ben ik veel telang mee bezig

--- Assets/scripts/OnEvenclick2.cs
+++ Assets/scripts/OnEvenclick2.cs
@@ -1,9 +1,11 @@
+using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using static UnityEngine.Rendering.DebugUI;
 
-public class OnEvenclick2 : MonoBehaviour, IPointerDownHandler, IPointerUpHandler,IPointerClickHandler,IPointerEnterHandler,IPointerExitHandler
+public class OnEvenclick2 : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private InputActionAsset input;
     [SerializeField] private string Action = "puzzle";
@@ -11,7 +13,11 @@ public class OnEvenclick2 : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
 
     private InputAction hold;
     public float b = 5;
+    public bool enterGameObject;
     [SerializeField] Vector3 dir;
+    public float speed = 10f;
+    public float speed2 = 0.3f;
+    public bool puzzlecomplyt = false;
     private void Awake()
     {
         map = input.FindActionMap(Action);
@@ -25,9 +31,58 @@ public class OnEvenclick2 : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
     {
         map.Disable();
     }
+    private void Update()
+    {
+
+        if (enterGameObject == true)
+        {
+
+            if (hold.IsPressed())
+            {
+                if (this.gameObject.tag == "keyPuzzel")
+                {
+
+                    Vector3 mouspos = Input.mousePosition;
+                    Debug.Log(mouspos);
+                    mouspos.z = 10.0f;
+
+                    Vector3 worldpos = Camera.main.ScreenToWorldPoint(mouspos);
+
+
+                    dir = (worldpos - transform.position).normalized;
+                    //float singleStep = speed * Time.deltaTime;
+                  
+                   
+
+                    Debug.Log(Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+                    float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+
+                    transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle,Vector3.forward), speed2 * Time.deltaTime * speed);
+
+
+                }
+
+            }
+        }
+        float zRot= transform.eulerAngles.z;
+        if (Mathf.Clamp(zRot, 0f, 5f) == zRot)
+        {
+            Debug.Log("asdasas");
+            if (puzzlecomplyt == false)
+            {
+                puzzlecomplyt = true;
+
+            }
+
+
+
+
+        }
+    }
     public void OnPointerDown(PointerEventData eventData)
     {
-      
+
     }
     public void OnPointerUp(PointerEventData eventData)
     {
@@ -39,28 +94,12 @@ public class OnEvenclick2 : MonoBehaviour, IPointerDownHandler, IPointerUpHandle
     }
     public void OnPointerEnter(PointerEventData eventData)
     {
-        if (hold.IsPressed())
-        {
-            Vector3 mouspos = Input.mousePosition;
-            //Debug.Log(mouspos);
-            mouspos.z = 10.0f;
-
-            Vector3 worldpos = Camera.main.ScreenToWorldPoint(mouspos);
-
-
-            dir = (worldpos - transform.position).normalized;
-
-            Debug.Log(Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
-            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-
-            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        }
+        enterGameObject = true;
     }
     public void OnPointerExit(PointerEventData eventData)
     {
-
+        enterGameObject = false;
     }
-    
+
 
 }
